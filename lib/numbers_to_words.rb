0433721334bf9fd0b2491cb8b607d7c4ea910c5b
Student <example@example.com>
2019-12-11 13:20:28 -0800

    array refactor

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,59 +1,29 @@
+def fixed_array(size, other)
+    Array.new(size) { |i| other[i] }
+end
+
 class Number
-
-    @@ones = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "elevin", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
-
-    @@tens = { 0 => "", 1 => "", 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
-    @@place= { 5 => "quadrillion, ", 4 => "trillion, ", 3 => "billion, ", 2 => "million, ", 1 => "thousand, ", 0 => "" }
+    @@ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "elevin", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    @@tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    @@place = ["", "thousand, ", "million, ", "billion, ", "trillion, ", "quadrillion, "]
 
     def initialize(number)
         @number = number
-        @digits = number.digits(1000)
+        @groups = number.digits(1000)
     end
 
-    def to_eng(i = (@digits.length - 1))
+    def to_eng(i = (@groups.length - 1))
         return "zero" if @number == 0
         temp = to_hundred(i)
-        ((i == 0) ? (temp) : (temp + to_eng(i - 1))).gsub(/,\s+\z/, "").gsub("  ", " ")
+        ((i == 0) ? (temp) : (temp + to_eng(i - 1))).strip.gsub(/,\z/, "").gsub("  ", " ")
     end
 
     def to_hundred(i)
-        return "" if (@digits[i] == 0)
-        hundos = (@digits[i]/100).floor
-        tens = @digits[i] - (hundos * (100))
-        eng = (hundos == 0) ? ("") : ("#{@@ones.fetch(hundos)} hundred ")
-        eng += ((tens < 20) ? "#{@@ones.fetch(tens)}" : "#{@@tens.fetch(tens.digits[1])} #{@@ones.fetch(tens.digits[0])}")
-        "#{eng} #{@@place.fetch(i)}"
+        return "" if (@groups[i] == 0)
+        digits = Array.new(3){ |j| @groups[i].digits[j] || 0 }
+
+        ((digits[2] == 0) ? "" : "#{@@ones[digits[2]]} hundred") +
+        ((digits[1] < 2) ? "#{@@ones[(digits[1]*10) + digits[0]]} " : (" #{@@tens[digits[1]]} #{@@ones[digits[0]]} ")) +
+        (@@place[i])
     end
-
-    # def translator
-    #     result_array = []
-    #     if @number == 0
-    #         result_array.push("zero")
-    #     elsif @number < 20
-    #         result_array.push(@@ones.fetch(@number))
-    #     elsif @number < 100
-    #         result_array.push(@@tens.fetch(@digits[1]))
-    #         result_array.push(@@ones.fetch(@digits[0]))
-    #     elsif @number < 1000
-    #         result_array.push(@@ones.fetch(@digits[2]))
-    #         result_array.push(@@base_place_number_words.fetch(@digits.length))
-    #         result_array.push(@@tens.fetch(@digits[1]))
-    #         result_array.push(@@ones.fetch(@digits[0]))
-    #     elsif @number < 10000
-    #         result_array.push(@@ones.fetch(@digits[3]))
-    #         result_array.push(@@base_place_number_words.fetch(@digits.length))
-    #         result_array.push(@@ones.fetch(@digits[2]))
-    #         result_array.push(@@base_place_number_words.fetch(@digits.length-1))
-    #         result_array.push(@@tens.fetch(@digits[1]))
-    #         result_array.push(@@ones.fetch(@digits[0]))
-    #     end
-    #     result_array.join(" ")
-    # end
-
 end
-
-number = Number.new(1123456789098765)
-puts number.to_eng
-
-
-# print 9000800070006.digits(1000)
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,34 +1,34 @@
 require('rspec')
 require('numbers_to_words.rb')
 
-describe('intiger#number') do
+describe('integer#number') do
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(0)
     expect(number.to_eng).to(eq("zero"))
   end
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(14)
     expect(number.to_eng).to(eq("fourteen"))
   end
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(24)
     expect(number.to_eng).to(eq("twenty four"))
   end
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(99)
     expect(number.to_eng).to(eq("ninety nine"))
   end
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(456)
     expect(number.to_eng).to(eq("four hundred fifty six"))
   end
 
-  it("returns a written-word version of the intiger") do
+  it("returns a written-word version of the integer") do
     number = Number.new(6789)
     expect(number.to_eng).to(eq("six thousand, seven hundred eighty nine"))
   end
